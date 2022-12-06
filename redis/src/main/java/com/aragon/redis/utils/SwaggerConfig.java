53a898108846ac3beafe5f5d26038526b64bf922
flyjing0 <845765654@qq.com>
2022-12-06 08:49:01 +0800

    2022-12-5_aragon_SwaggerConfig

---
+++ redis/src/main/java/com/aragon/redis/utils/SwaggerConfig.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (C), 2019-2022, XXX有限公司
+ * FileName: SwaggerConfig
+ * Author:   Administrator
+ * Date:     2022/12/5 17:34
+ * Description: swagger2配置类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.aragon.redis.utils;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * 〈功能简述〉<br> 
+ * 〈swagger2配置类〉
+ *
+ * @author Administrator
+ * @create 2022/12/5
+ * @since 1.0.0
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                //.apis(RequestHandlerSelectors.withClassAnnotation(Api.class))//这是注意的代码
+                .apis(RequestHandlerSelectors.basePackage("com.aragon.redis"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Aragon的接口文档")
+                .description("reids接口的文档")
+                .termsOfServiceUrl("http://www.xxx.com")
+                .version("1.1.0")
+                .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui/**").addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/");
+        super.addResourceHandlers(registry);
+    }
+
+}
